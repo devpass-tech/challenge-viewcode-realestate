2773b653bacc8596409edcffedf78cee9abca480
Rodrigo Borges <rborges.soares@gmail.com>
2021-10-14 17:48:15 -0300

    Modificações no projeto base e criação de novas pastas para sprints

---
+++ solutions/devsprint-viewcode-2/ViewCodeChallenge-RealEstate/Screens/PropertyList/PropertyListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewCodeChallenge-RealEstate
+//
+//  Created by Rodrigo Borges on 29/09/21.
+//
+
+import UIKit
+
+class PropertyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+
+        let apiClient = RealEstateAPIClient()
+
+        apiClient.fetchProperties { properties in
+
+            print(properties)
+        }
+    }
+}
+
