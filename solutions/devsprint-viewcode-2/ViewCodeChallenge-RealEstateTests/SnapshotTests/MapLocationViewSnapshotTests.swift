5f9c00182236afdf5b9040afac10b0ffbe5c43b0
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-10-20 21:58:04 -0300

    adicionado maplocation e testes

---
+++ solutions/devsprint-viewcode-2/ViewCodeChallenge-RealEstateTests/SnapshotTests/MapLocationViewSnapshotTests.swift
@@ -0,0 +1,42 @@
+//
+//  MapLocationViewSnapshotTests.swift
+//  ViewCodeChallenge-RealEstateTests
+//
+//  Created by Alexandre Cardoso on 20/10/21.
+//
+
+import XCTest
+import CoreLocation
+import SnapshotTesting
+
+@testable import ViewCodeChallenge_RealEstate
+
+class MapLocationViewSnapshotTests: XCTestCase {
+	
+	var sut: MapLocationView!
+	var viewModel: MapLocationViewModel!
+	
+	override func setUp() {
+		super.setUp()
+		
+		let frame = CGRect(x: 0, y: 0, width: 375, height: 150)
+		sut = MapLocationView(frame: frame)
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+		sut = nil
+		viewModel = nil
+	}
+
+	func testMapLocationView() {
+		let initialLocation = CLLocationCoordinate2D(latitude: -23.554389, longitude: -46.608185)
+//		let initialLocation = CLLocationCoordinate2D(latitude: 21.282778, longitude: -157.829444)
+		let viewModel = MapLocationViewModel(coordinate: initialLocation)
+		
+		sut.configure(with: viewModel)
+		
+		assertSnapshot(matching: sut, as: .wait(for: 10, on: .image), record: false)
+	}
+
+}
