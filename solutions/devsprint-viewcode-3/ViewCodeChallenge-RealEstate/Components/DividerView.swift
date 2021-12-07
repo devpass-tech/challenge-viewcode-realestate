0ff60de85b87c0402751d39eb7ef941d079f34be
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-12-07 15:02:52 -0300

    feat: add DividerView component and its preview

---
+++ solutions/devsprint-viewcode-3/ViewCodeChallenge-RealEstate/Components/DividerView.swift
@@ -0,0 +1,69 @@
+//
+//  DividerView.swift
+//  ViewCodeChallenge-RealEstate
+//
+//  Created by Guilherme Strutzki on 07/12/21.
+//
+
+import UIKit
+
+struct DividerViewConfiguration {
+    let color: UIColor?
+    let height: CGFloat?
+}
+
+final class DividerView: UIView {
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        self.setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with configuration: DividerViewConfiguration) {
+        dividerView.backgroundColor = configuration.color ?? .lightGray
+        NSLayoutConstraint.activate([
+            dividerView.heightAnchor.constraint(equalToConstant: configuration.height ?? 2)
+        ])
+    }
+}
+
+extension DividerView: ViewCode {
+    func configureSubviews() {
+        addSubview(dividerView)
+    }
+    
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+            dividerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    func configureAdditionalBehaviors() {}
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct FeaturePreviews: PreviewProvider {
+    static var previews: some View {
+        UIViewPreview {
+            let dividerView = DividerView()
+            dividerView.configure(with: DividerViewConfiguration(color: .blue,
+                                                                 height: 2))
+            return dividerView
+        }
+        .previewLayout(.fixed(width: UIScreen.main.bounds.width,
+                              height: 2))
+    }
+}
+#endif
