4221cb2e35350f2bbbf2f8d4289689555a1b368a
Pedro Gabriel <pee.torres@gmail.com>
2021-12-08 20:19:10 -0300

    Started Snapshot Tests of ButtonView

---
+++ solutions/devsprint-viewcode-3/ViewCodeChallenge-RealEstateTests/SnapshotTests/ButtonViewSnapshotTests.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonViewSnapshotTests.swift
+//  ViewCodeChallenge-RealEstateTests
+//
+//  Created by Pedro Gabriel on 08/12/21.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import ViewCodeChallenge_RealEstate
+
+class ButtonViewSnapshotTests: XCTestCase {
+    func test_buttonDefaultState() {
+        let buttonView = ButtonView()
+        buttonView.configure(with: .init(title: "Ver mais"))
+        let result = verifySnapshot(matching: buttonView, as: Snapshotting.image(size: CGSize(width: 360, height: 50)))
+        XCTAssertNil(result)
+    }
+}
