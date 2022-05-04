5f2a5e7edc2e84c776b81c55ffdf6a8896ff4360
Anderson Oliveira <anderson.oliveira@cora.com.br>
2022-05-04 09:01:47 -0300

    added test about DividerView

---
+++ solutions/devsprint-matheus-reis-1/RealEstateAppTests/Components/DividerViewTest.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  RealEstateAppTests
+//
+//  Created by Cora on 04/05/22.
+//
+
+import Foundation
+import XCTest
+//import SnapshotTesting
+
+@testable import RealEstateApp
+import SnapshotTesting
+
+
+class DividerViewTest: XCTestCase {
+    
+    var sut =  DividerView()
+    
+    override func setUpWithError() throws {
+       
+    }
+
+    func testDefaultState() {
+        assertSnapshot(matching: sut, as: .image)
+    }
+}
