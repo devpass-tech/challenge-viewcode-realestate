3912ea4db044de57ab22ba8c7ad17b9c43da0a42
Anderson Oliveira <anderson.oliveira@cora.com.br>
2022-05-06 07:27:16 -0300

    requests fixed

--- solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Screens/PropertyDescription/PropertyDescriptionView.swift
+++ solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Screens/PropertyDescription/PropertyDescriptionView.swift
@@ -15,22 +15,24 @@ protocol PropertyDescriptionViewProtocol {
 
 final class PropertyDescriptionView: UIView, PropertyDescriptionViewProtocol, ViewCode {
     
-   private let divider = DividerView()
-
+    private let divider = DividerView()
+    
     private lazy var titleLabel: UILabel = {
         let title = UILabel()
         title.text = "Descrição"
         title.font = .systemFont(ofSize: 22, weight: .bold)
+        title.translatesAutoresizingMaskIntoConstraints = false
         return title
     }()
     
     private lazy var descriptionLabel: UILabel = {
-       let description = UILabel()
+        let description = UILabel()
         description.numberOfLines = 0
         description.font = .systemFont(ofSize: 20)
+        description.translatesAutoresizingMaskIntoConstraints = false
         return description
     }()
-
+    
     init(descriptionLabel: String){
         super.init(frame: .zero)
         setupViews()
@@ -40,7 +42,7 @@ final class PropertyDescriptionView: UIView, PropertyDescriptionViewProtocol, Vi
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func updateDescription(description: String) {
         descriptionLabel.text = description
     }
@@ -54,16 +56,16 @@ final class PropertyDescriptionView: UIView, PropertyDescriptionViewProtocol, Vi
     }
     
     func configureAdditionalBehaviors() {
-        [titleLabel,divider,descriptionLabel].forEach {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
+//        [titleLabel,divider,descriptionLabel].forEach {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//        }
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 12),
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
- 
+            
             divider.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
-
+            
             descriptionLabel.topAnchor.constraint(equalTo: divider.bottomAnchor, constant: 16),
             descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
