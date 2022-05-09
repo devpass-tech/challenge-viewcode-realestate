50412199c312257059aefade50c60f56e848b6f8
Mariana Maiko <marimaiko@cora.com.br>
2022-05-09 18:37:57 -0300

    constraints corrected

--- solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Screens/PropertyDetails/Components/ContactFormView.swift
+++ solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Screens/PropertyDetails/Components/ContactFormView.swift
@@ -9,18 +9,20 @@ import UIKit
 
 class ContactFormView: UIView, ViewCode {
      
-    lazy var title: UILabel = {
+    lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Tem interesse?"
+        label.textColor = .darkText
+        label.font = .systemFont(ofSize: 20, weight: .bold)
         
         return label
     }()
     
-    lazy var name: UITextField = {
+    lazy var nameTextField: UITextField = {
         let textfield = UITextField()
         textfield.translatesAutoresizingMaskIntoConstraints = false
-        textfield.placeholder = "Name"
+        textfield.placeholder = "  Nome"
         textfield.layer.borderColor = UIColor.lightGray.cgColor
         textfield.layer.borderWidth = 0.8
         textfield.layer.cornerRadius = 2
@@ -28,10 +30,10 @@ class ContactFormView: UIView, ViewCode {
         return textfield
     }()
     
-    lazy var email: UITextField = {
+    lazy var emailTextField: UITextField = {
         let textfield = UITextField()
         textfield.translatesAutoresizingMaskIntoConstraints = false
-        textfield.placeholder = "Email"
+        textfield.placeholder = "  Email"
         textfield.layer.borderColor = UIColor.lightGray.cgColor
         textfield.layer.borderWidth = 0.8
         textfield.layer.cornerRadius = 2
@@ -39,7 +41,7 @@ class ContactFormView: UIView, ViewCode {
         return textfield
     }()
     
-    lazy var button: UIButton = {
+    lazy var sendbutton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .blue
@@ -63,25 +65,28 @@ class ContactFormView: UIView, ViewCode {
     }
     
     func configureSubviews() {
-        [title, name, email, button].forEach(addSubview)
+        [titleLabel, nameTextField, emailTextField, sendbutton].forEach(addSubview)
     }
     
     func configureSubviewsConstraints() {
         NSLayoutConstraint.activate([
-            title.topAnchor.constraint(equalTo: topAnchor, constant: 12),
-            title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
             
-            name.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 36),
-            name.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            name.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            nameTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 36),
+            nameTextField.heightAnchor.constraint(equalToConstant: 36),
+            nameTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            nameTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
             
-            email.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 24),
-            email.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            email.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 24),
+            emailTextField.heightAnchor.constraint(equalToConstant: 36),
+            emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
             
-            button.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 24),
-            button.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            button.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12)
+            sendbutton.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 24),
+            sendbutton.heightAnchor.constraint(equalToConstant: 42),
+            sendbutton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            sendbutton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12)
         ])
     }
 }
