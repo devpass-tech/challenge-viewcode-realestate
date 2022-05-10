f85a8ab58efe13d86623f96f579381d9fd8d1972
Luiza Moruz <luizamoruz@cora.com.br>
2022-05-10 10:42:49 -0300

    Implement LoginView - WIP

---
+++ solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Screens/Account/LoginView.swift
@@ -0,0 +1,110 @@
+//
+//  LoginView.swift
+//  RealEstateApp
+//
+//  Created by Cora on 10/05/22.
+//
+
+import UIKit
+
+// setar title viewdidload da viewcontroller para Account
+// title label login with your account
+// dois text fields (com placeholder)
+//  um button
+// forget password
+// tudo em stack view
+// chamar o email.becomeFirstResponder() na viewcontroller, depois da view carregar
+
+final class LoginView: UIView, ViewCode {
+    
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .fill
+        stack.spacing = 20
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var loginTitleLabel: UILabel = {
+        let title = UILabel()
+        title.font = .systemFont(ofSize: 15, weight: .bold)
+        title.text = "Login with your account"
+    
+        title.translatesAutoresizingMaskIntoConstraints = false
+        return title
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let email = UITextField()
+        email.placeholder = "Email"
+        email.layer.borderColor = UIColor.lightGray.cgColor
+        email.layer.borderWidth = 0.8
+        email.layer.cornerRadius = 5
+        email.keyboardType = .emailAddress
+        email.layer.masksToBounds = true
+        email.translatesAutoresizingMaskIntoConstraints = false
+        return email
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let password = UITextField()
+        password.placeholder = "Password"
+        password.layer.borderColor = UIColor.lightGray.cgColor
+        password.layer.borderWidth = 0.8
+        password.layer.cornerRadius = 5
+        password.layer.masksToBounds = true
+        password.isSecureTextEntry = true
+        password.translatesAutoresizingMaskIntoConstraints = false
+        return password
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.configuration?.background.cornerRadius = 5
+        button.setTitle("Login", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton(configuration: .plain())
+        button.configuration?.background.cornerRadius = 5
+        button.setTitle("Forgot password", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureSubviews() {
+        [loginTitleLabel, emailTextField, passwordTextField, loginButton, forgotPasswordButton].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        addSubview(stackView)
+    }
+    
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 100),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            emailTextField.heightAnchor.constraint(equalToConstant: 30),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    func configureAdditionalBehaviors() {
+        backgroundColor = .white
+    }
+}
+
