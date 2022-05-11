63087d1b77a88b8a1e3cc882a27377c29fbc76c2
Mariana Maiko <marimaiko@cora.com.br>
2022-05-11 16:22:28 -0300

    label and activityIndicator added into subview

--- solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Components/LoadingView.swift
+++ solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Components/LoadingView.swift
@@ -8,6 +8,58 @@
 import Foundation
 import UIKit
 
-class LoadingView: UIView {
+class LoadingView: UIView, ViewCode{
     
+    private lazy var titleLabel: UILabel = {
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.text = "Searching for listings..."
+        title.textColor = .black
+        title.font = .systemFont(ofSize: 18, weight: .bold)
+        
+        return title
+    }()
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .gray)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.startAnimating()
+        
+        return indicator
+    }()
+    
+    private lazy var stackview: UIStackView = {
+        let stackview = UIStackView()
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.axis = .vertical
+        stackview.alignment = .center
+        stackview.spacing = 16
+        
+        return stackview
+    }()
+    
+    init(){
+        super.init(frame: .zero)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureAdditionalBehaviors() {
+        backgroundColor = .white
+    }
+    
+    func configureSubviews() {
+        addSubview(stackview)
+        [titleLabel, activityIndicator].forEach(stackview.addArrangedSubview)
+    }
+    
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+            stackview.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackview.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
 }
