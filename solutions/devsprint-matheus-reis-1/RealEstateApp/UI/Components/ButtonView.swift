a18588fd6f6382d7bb93425732040709cae97f4e
Mariana Maiko <marimaiko@cora.com.br>
2022-05-13 14:15:48 -0300

    Test fixed

--- solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Components/ButtonView.swift
+++ solutions/devsprint-matheus-reis-1/RealEstateApp/UI/Components/ButtonView.swift
@@ -8,10 +8,6 @@
 import Foundation
 import UIKit
 
-struct ButtonViewConfiguration {
-    var buttonText: String
-}
-
 class ButtonView: UIButton, ViewCode {
     
     init() {
@@ -23,25 +19,14 @@ class ButtonView: UIButton, ViewCode {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func updateView(with configuration: ButtonViewConfiguration) {
-        setTitle(configuration.buttonText, for: .normal)
-    }
-    
     func configureAdditionalBehaviors() {
-        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .blue
         layer.cornerRadius = 8
     }
     
     func configureSubviews() {
-        
     }
     
     func configureSubviewsConstraints() {
-        NSLayoutConstraint.activate([
-            leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
-            heightAnchor.constraint(equalToConstant: 42),
-        ])
     }
 }
--- solutions/devsprint-matheus-reis-1/RealEstateAppTests/Components/ButtonViewTest.swift
+++ solutions/devsprint-matheus-reis-1/RealEstateAppTests/Components/ButtonViewTest.swift
@@ -15,6 +15,7 @@ class ButtonViewTest: XCTestCase {
     var sut =  ButtonView()
 
     func testButtonView() {
+        sut.setTitle("Ver mais", for: .normal)
         assertSnapshot(matching: sut, as: .image(size: CGSize(width: 414, height: 100 )))
     }
 }
